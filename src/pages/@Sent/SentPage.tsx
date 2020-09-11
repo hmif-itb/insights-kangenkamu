7ade1b0936ac3756a7ff9c82bc73f7e4aa840635
didithilmy <didithilmy@gmail.com>
2020-09-11 20:03:59 +0700

    Add sent and welcome content

--- src/pages/@Sent/SentPage.tsx
+++ src/pages/@Sent/SentPage.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
 
 const SentPage: React.FC = () => {
     return (
         <div>
-            <h1>Message sent!</h1>
+            <Container maxWidth="sm">
+                <Box p={2} display="flex" flexDirection="column" justifyContent="center" style={{height: '100vh', boxSizing: 'border-box'}}>
+                    <h1><span className="text-yellow">Pesan terkirim!</span></h1>
+                    <p>
+                        Pesan kamu udah kami simpan dan akan diberikan kepada penerima di akhir tahun.
+                        Nantikan kejutan kami ya!
+                    </p>
+                    <Box flex={1}></Box>
+                    <Button color="primary" variant="contained" size="large" component={RouterLink} to="/compose">
+                        Kirim pesan lagi
+                    </Button>
+                    <Box mt={4}></Box>
+                </Box>
+            </Container>
         </div>
     );
 }
--- src/pages/@Welcome/WelcomePage.tsx
+++ src/pages/@Welcome/WelcomePage.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 import GoogleLogin from "react-google-login";
 
 const WelcomePage: React.FC = () => {
@@ -11,13 +13,37 @@ const WelcomePage: React.FC = () => {
 
     return (
         <div>
-            <GoogleLogin
-                clientId={googleClientId}
-                hostedDomain={googleHostedDomain}
-                buttonText="Login to std.stei.itb.ac.id"
-                uxMode="redirect"
-                redirectUri={redirectUrl}
-            />
+            <Container maxWidth="sm">
+                <Box p={2} display="flex" flexDirection="column" justifyContent="stretch">
+                    <h1><span className="text-yellow">#KangenKamu</span> edisi website</h1>
+                    <p>
+                        HALO WARGA HMIF!!
+                    </p>
+                    <p>
+                        Jadi dari DE HMIF lagi nyiapin sesuatu yang spesial di akhir tahun nanti.
+                        Nah biar seru, kita bakal adain #KangenKamu dengan format yang agak beda nih.
+                    </p>
+                    <p>
+                        Di #KangenKamu kali ini, kamu bisa menuliskan pesan apapun kepada warga HMIF angkatan 2016 s.d. 2019, anonim maupun tidak.
+                        Kalo kamu mau anonim, admin pun gak bakal bisa tau pesannya dikirim dari siapa.
+                        Jadi kalo ada kesan, pesan, pengakuan dosa, apapun, bisa banget ditulis di sini.
+                    </p>
+                    <p>
+                        Pesan kamu akan kami liatin ke penerima pesan <b>di akhir tahun nanti</b>. Caranya gimana? Itu kejutan, liat aja nanti.
+                    </p>
+                    <p>
+                        Silakan login pake akun email std.stei.itb.ac.id untuk mengirim pesanmu.
+                    </p>
+                    <Box mt={1}></Box>
+                    <GoogleLogin
+                        clientId={googleClientId}
+                        hostedDomain={googleHostedDomain}
+                        buttonText="Login to std.stei.itb.ac.id"
+                        uxMode="redirect"
+                        redirectUri={redirectUrl}
+                    />
+                </Box>
+            </Container>
         </div>
     );
 }

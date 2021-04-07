a041e1792e50f594e92c6496835b056f7db18906
Muhammad Aditya Hilmy <mhilmy@hey.com>
2021-04-07 21:34:38 +0700

    Comment unused

--- src/pages/@Welcome/WelcomePage.tsx
+++ src/pages/@Welcome/WelcomePage.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
-import GoogleLogin from "react-google-login";
+// import GoogleLogin from "react-google-login";
 
 const WelcomePage: React.FC = () => {
-    const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
-    const googleHostedDomain = process.env.REACT_APP_GSUITE_DOMAIN;
+    // const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
+    // const googleHostedDomain = process.env.REACT_APP_GSUITE_DOMAIN;
 
-    const location = window.location;
-    const baseUrl = location.protocol + "//" + location.host;
-    const redirectUrl = baseUrl + '/login'
+    // const location = window.location;
+    // const baseUrl = location.protocol + "//" + location.host;
+    // const redirectUrl = baseUrl + '/login'
 
     return (
         <div>
